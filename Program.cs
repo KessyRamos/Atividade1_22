c05ce31681e4adf3896f308777e3245ce45fc184
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:13:56 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int idadeMin = 20;
+int idadeMax = 50;
+int mediaIdade = (idadeMin + idadeMax)/2;
+
+Console.WriteLine("Calcular média de idade dos alunos!!!");
+
+Console.Write($"A idade média dos alunos é de: {mediaIdade}");
